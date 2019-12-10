f212a4fb4c5efce9759638518c104a28af788afe
everlen <victor.everlen@gmail.com>
2019-12-10 20:49:15 -0300

    Criação das models cadastro e contato

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from website.models import Cadastro, Contato
 
 # Register your models here.
+admin.site.register(Cadastro)
+admin.site.register(Contato)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Cadastro(models.Model):
+    nome = models.CharField(max_length=140)
+    email = models.EmailField()
+    cpf = models.IntegerField()
+    dataN = models.DateField()
+    mae = models.CharField(max_length=140)
+    opcoes_plano = [
+        ('diario', 'Diarias'),
+        ('semanal', 'semanal'),
+        ('Mensal', 'Mensal'),
+        ('Anual', 'Anual'),
+    ]
+    login = models.CharField(max_length=15, default= "1111")
+    senha = models.CharField(max_length=15, default= "1111")
+
+    def __str__(self):
+        return self.nome
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=140)
+    email = models.EmailField()
+    assunto = models.CharField(max_length=30)
+    messagem = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.nome
