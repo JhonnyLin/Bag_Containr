ea8bdda5c9f3f80e68757667a256132ea8d5abe8
gabriel neres <biel.neres11@hotmail.com>
2019-12-10 23:30:27 -0300

    Retirando POST da page admin.py

--- website/admin.py
+++ website/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from website.models import Cadastro, Contato
-from .models import Post
 
 # Register your models here.
 admin.site.register(Cadastro)
 admin.site.register(Contato)
-admin.site.register(Post)
